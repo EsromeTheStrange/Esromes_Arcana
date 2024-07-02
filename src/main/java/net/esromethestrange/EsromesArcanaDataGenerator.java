bd0ddea632845e2484a91ad85d30f4a303b314ac
Esrome The Strange <esromethestrange@gmail.com>
2024-07-02 10:43:38 +0900

    Project Template

---
+++ src/main/java/net/esromethestrange/EsromesArcanaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.esromethestrange;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EsromesArcanaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
